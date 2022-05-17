676b1a6891ecb7bd3744c298415c61ae12ab64c7
Juan Sáez García <jsaez@odec.es>
2022-05-17 14:57:29 +0200

    Subscription y unsubscribe 1/2

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import { Observable, Observer } from "rxjs";
 
 const observer: Observer<string> = {
     next: (res) => console.log("next: ", res),
-    error: (err) => console.error("error: ", err),
+    error: (err) => console.warn("error: ", err),
     complete: () => console.info('complete')
 }
+
+const intervalo$: Observable<number> = new Observable<number>(susb=>{
+    let contador: number = 0;
+    setInterval(()=>{
+        susb.next(contador);
+        contador++;
+    },1000)
+});
+intervalo$.subscribe(console.log)
