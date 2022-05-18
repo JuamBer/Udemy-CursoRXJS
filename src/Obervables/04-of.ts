a767049a9831a4a84ca7e7ab058d8bbf07388780
Juan Sáez García <jsaez@odec.es>
2022-05-18 10:58:08 +0200

    fromEvent

---
+++ src/Obervables/04-of.ts
@@ -0,0 +1,14 @@
+import { Observable, Observer, Subject, Subscription, of } from "rxjs";
+
+const observer: Observer<any> = {
+    next: (res) => console.log("next: ", res),
+    error: (err) => console.warn("error: ", err),
+    complete: () => console.info('complete')
+}
+
+//const obs$ = of(1,2,3,4,5,6,7);
+const obs$ = of([1, 2], { a: 1, b: 2 }, function () { }, Promise.resolve(true));
+
+console.log("Inicio Obs");
+obs$.subscribe(observer);
+console.log("Fin Obs");
